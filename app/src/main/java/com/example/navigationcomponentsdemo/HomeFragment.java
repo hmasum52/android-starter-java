99ccb01845194999ef3abcb47c9902bc0b7c120b
Hasan Masum <1805052@ugrad.cse.buet.ac.bd>
2023-09-21 22:54:03 +0600

    part1 and part2 done

---
+++ app/src/main/java/com/example/navigationcomponentsdemo/HomeFragment.java
@@ -0,0 +1,40 @@
+package com.example.navigationcomponentsdemo;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.navigationcomponentsdemo.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    String name;
+
+    FragmentHomeBinding binding;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            name = getArguments().getString("name");
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        String msg= "Hello "+name;
+        binding.greetingEdt.setText(msg);
+    }
+}
---
+++ app/src/main/java/com/example/navigationcomponentsdemo/WelcomeFragment.java
@@ -0,0 +1,44 @@
+package com.example.navigationcomponentsdemo;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.navigationcomponentsdemo.databinding.FragmentWelcomeBinding;
+
+
+public class WelcomeFragment extends Fragment {
+
+    private FragmentWelcomeBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        binding.nextBtn.setOnClickListener(v -> {
+            String name = binding.nameEdt.getText().toString();
+            if(name.isEmpty()){
+                binding.nameEdt.setError("Please enter your name");
+                return;
+            }
+
+            Bundle bundle = new Bundle();
+            bundle.putString("name", name);
+
+            NavHostFragment.findNavController(this).navigate(R.id.action_welcomeFragment_to_homeFragment, bundle);
+        });
+    }
+}
