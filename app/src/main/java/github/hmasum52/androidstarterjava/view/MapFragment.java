6788573dc4a39b2cc50906a8dc797ed3f26cf4a2
Hasan Masum <1805052@ugrad.cse.buet.ac.bd>
2024-05-08 03:27:18 +0600

    feat: add google map

--- app/src/main/java/github/hmasum52/androidstarterjava/view/MapFragment.java
+++ app/src/main/java/github/hmasum52/androidstarterjava/view/MapFragment.java
@@ -1,6 +1,7 @@
 package github.hmasum52.androidstarterjava.view;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,16 +10,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 
 import javax.inject.Inject;
 
 import dagger.hilt.android.AndroidEntryPoint;
+import github.hmasum52.androidstarterjava.R;
 import github.hmasum52.androidstarterjava.databinding.FragmentMapBinding;
 import github.hmasum52.androidstarterjava.util.GPS;
 
 @AndroidEntryPoint
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
     @Inject
     GPS gps;
 
@@ -32,11 +38,30 @@ public class MapFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        gps.requestDeviceLocation(latLng -> {
-            String msg = "Your current location: " + latLng.latitude + ", " + latLng.longitude;
-            binding.tv.setText(msg);
-        });
+    public void onResume() {
+        super.onResume();
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        if(mapFragment == null){
+            Log.e("MapFragment", "onResume: mapFragment is null");
+            return;
+        }
+        try {
+            mapFragment.getMapAsync(this);
+        } catch (Exception e) {
+            Log.e("MapFragment", "onResume: " + e.getMessage());
+        }
     }
 
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) throws SecurityException {
+        binding.fabMyLocation.setOnClickListener(v -> {
+            gps.requestDeviceLocation(latLng -> {
+                googleMap.setMyLocationEnabled(true);
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+            });
+        });
+
+    }
 }
